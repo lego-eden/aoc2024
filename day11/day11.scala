79c9e75c5808b9a470cabcec66e34306cb9e9916
lego-eden <hampus.eden@gmail.com>
2024-12-12 00:02:30 +0100

    switch to Long

--- day11/day11.scala
+++ day11/day11.scala
@@ -2,7 +2,7 @@ import scala.collection.mutable.HashMap as MutMap
 
 object day11 extends Day:
 
-  case class Stone(num: BigInt):
+  case class Stone(num: Long):
     override lazy val toString: String = num.toString
     lazy val digitCount = toString.size  
 
@@ -10,14 +10,14 @@ object day11 extends Day:
       if num == 0 then Vector(Stone(1))
       else if digitCount.isEven then
         toString.splitAt(digitCount / 2)
-          .map(s => Stone(BigInt(s)))
+          .map(s => Stone(s))
           .toList
           .toVector
       else
         Vector(Stone(num * 2024))
 
-    val blinkN: Int => BigInt =
-      def blinkN(n: Int): BigInt =
+    val blinkN: Int => Long =
+      def blinkN(n: Int): Long =
         if n <= 0 then
           1
         else 
@@ -26,18 +26,18 @@ object day11 extends Day:
       n => Stone.blinkMemo.getOrElseUpdate((this, n), blinkN(n))
   
   object Stone:
-    private val blinkMemo = MutMap.empty[(Stone, Int), BigInt]
-    def apply(s: String): Stone = new Stone(BigInt(s))
+    private val blinkMemo = MutMap.empty[(Stone, Int), Long]
+    def apply(s: String): Stone = new Stone(s.toLong)
 
   extension (xs: Vector[Stone])
-    def blink(n: Int): BigInt =
+    def blink(n: Int): Long =
       xs.map(_.blinkN(n)).sum
 
   def parseInput(lines: IndexedSeq[String]): Vector[Stone] =
     lines.head.split(" ").map(s => Stone(s)).toVector
   
-  override def partOne(lines: IndexedSeq[String]): BigInt =
+  override def partOne(lines: IndexedSeq[String]): Long =
     parseInput(lines).blink(25)
 
-  override def partTwo(lines: IndexedSeq[String]): BigInt =
+  override def partTwo(lines: IndexedSeq[String]): Long =
     parseInput(lines).blink(75)
